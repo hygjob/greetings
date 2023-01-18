343805bc4cd324b2de6bb30f293ac64972b965f6
hyounggo <hyounggo@unioncomm.co.kr>
2023-01-18 10:58:52 +0900

    bug fix

--- single/single.go
+++ single/single.go
@@ -3,11 +3,12 @@ package single
 import (
     "errors"
     "fmt"
-    "math/rand"    
+    "math/rand"
+    "time"    
 )
 
 // Hello returns a greeting for the named person.
-func Hello(name string, short bool) (string, error) {
+func Hello(name string, userand bool) (string, error) {
     // If no name was given, return an error with a message.
     if name == "" {
         return "", errors.New("empty name")
@@ -16,10 +17,34 @@ func Hello(name string, short bool) (string, error) {
     // If a name was received, return a value that embeds the name
     // in a greeting message.
     //message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    message := fmt.Sprintf(randomFormat(short), name)
+    message := fmt.Sprintf(randomFormat(userand), name)
     return message, nil
 }
 
+// init sets initial values for variables used in the function.
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat(userand bool) string {
+    // A slice of message formats.
+    formats := []string{
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+
+    // Return a randomly selected message format by specifying
+    // a random index for the slice of formats.
+    //return formats[rand.Intn(len(formats))]
+    if userand == true {
+        return formats[rand.Intn(len(formats))]
+    } 
+    return format[0]
+    
+}
 /*
 func Hello(name string) (string, error) {
     // If no name was given, return an error with a message.
